96087c2abf636c4ea34dbc04c0612c4a0c6bc866
familiagomeznavalmoral <familiagomeznavalmoral@gmail.com>
2026-03-07 16:32:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
+// ==========================
+// EFECTO NAVBAR AL SCROLL
+// ==========================
+
 window.addEventListener('scroll', function() {
+
     const nav = document.querySelector('#navbar');
+
     if (window.scrollY > 100) {
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
     }
+
+});
+
+
+// ==========================
+// ANIMACION AL HACER SCROLL
+// ==========================
+
+window.addEventListener("scroll", function(){
+
+    const elements = document.querySelectorAll(".villa-card");
+
+    elements.forEach(function(el){
+
+        const position = el.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight;
+
+        if(position < screenPosition - 100){
+
+            el.classList.add("show");
+
+        }
+
+    });
+
 });
